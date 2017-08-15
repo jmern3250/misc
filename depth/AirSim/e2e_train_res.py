ac7ee60fca46f112baa806c785e7327ac789887b
jmern3250 <jmern91@stanford.edu>
2017-08-15 13:50:35 -0700

    bug fix

--- depth/AirSim/e2e_train_res.py
+++ depth/AirSim/e2e_train_res.py
@@ -71,7 +71,7 @@ def main(args):
     #           writer=writer, sum_vars=merged)
 
     _ = run_model(sess, X, Y, is_training, mean_loss, X_train, Y_train, 
-              epochs=args.epochs, batch_size=args.batch_size, 
+              epochs=args.l1_epochs, batch_size=args.batch_size, 
               print_every=10, training=train_l1, plot_losses=False,
               writer=writer, sum_vars=merged)
 
