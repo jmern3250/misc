error scaling fix

depth/AirSim/discriminator_cont_train.py
@@ -51,7 +51,7 @@ def main(args):
     trans_loss = l1_norm(output-Y)
     reg_loss = TV_loss(output)
 
-    mean_loss = tf.reduce_mean(trans_loss + 0.1*reg_loss + 10.0*disc_val_x)
+    mean_loss = tf.reduce_mean(0.0*trans_loss + 0.0*reg_loss + 1.0*disc_val_x)
     # mean_loss = tf.reduce_mean(trans_loss + 0.1*reg_loss)
     tf.summary.scalar('loss', mean_loss)
 
@@ -73,9 +73,9 @@ def main(args):
     writer = tf.summary.FileWriter('./tb',sess.graph)
 
     sess.run(tf.global_variables_initializer())
-    enc_saver.restore(sess, './disc_model/initial_model_enc')
-    dec_saver.restore(sess, './disc_model/initial_model_dec')
-    disc_saver.restore(sess, './disc_model/initial_model_disc')
+    enc_saver.restore(sess, './disc_model/final_model_enc')
+    dec_saver.restore(sess, './disc_model/final_model_dec')
+    disc_saver.restore(sess, './disc_model/final_model_disc')
     _ = run_model(sess, X, Y, is_training, disc_val, mean_loss, X_train, Y_train, 
               epochs=args.epochs, batch_size=args.batch_size, print_every=10,
               disc_training=train_discriminator, gen_training=train_generator, 