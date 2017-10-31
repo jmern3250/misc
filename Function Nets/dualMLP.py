32c8fa988f93943f64b6757e681e0606498faacc
jmern3250 <jmern91@stanford.edu>
2017-10-30 18:09:23 -0700

    ugh

--- Function Nets/dualMLP.py
+++ Function Nets/dualMLP.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import tensorflow as tf 
 import itertools 
+import os
 
 import pdb
 
@@ -9,20 +10,20 @@ def encoder(X, sizes, bits=8):
 	layers[0] = tf.layers.dense(X, 
 								sizes[0],
 								activation=tf.nn.relu,
-								use_bias=False
+								use_bias=True
 								) 
 	for j, size in enumerate(sizes[1:]):
 		i = j+1
 		layers[i] = tf.layers.dense(layers[i-1], 
 								size,
 								activation=tf.nn.relu,
-								use_bias=False
+								use_bias=True
 								) 
 
 	binary = tf.layers.dense(layers[len(sizes)-1], 
 									bits,
                                     activation = tf.tanh,
-                                    use_bias=False,
+                                    use_bias=True,
                                     ) 
 	return binary 
 
@@ -33,31 +34,31 @@ def decoder(binary):
 def maxset(B):
 	span = np.ones([0,B])
 	i = 0
-	for row in itertools.product([-1,1], repeat=8):
+	for row in itertools.product([-1,1], repeat=B):
 		span = np.vstack([span,row])
 		i += 1
 	return span 
 
-def solveB(X, W, c, Bmax):
-	def loss(X, W, c, b):
+def solveB(H, X, W, c, Bmax, nu):
+	def loss(H, X, W, c, b, nu):
 		x_hat = W.T.dot(b) + c
-		# pdb.set_trace()
-		loss = np.linalg.norm(X - x_hat)**2
+		loss = np.linalg.norm(X - x_hat)**2 + nu*np.linalg.norm(b - H)**2
 		return loss
-
-	Bloss = []
-	for b in Bmax: 
-		bloss = loss(X,W,c,b)
-		Bloss.append(bloss)
-	idx = np.argmin(Bloss)
-	B = Bmax[idx]
+	B = np.zeros([0,Bmax.shape[1]])
+	for i, x in enumerate(X): 
+		Bloss = []
+		for b in Bmax: 
+			bloss = loss(H[i,:],x,W,c,b,nu)
+			Bloss.append(bloss)
+		idx = np.argmin(Bloss)
+		B = np.vstack([B, Bmax[idx]])
 	return B 
 
-SIZES = [256, 512, 256]
+SIZES = [512, 512, 512]
 BITS = 8
-INTS_OUT = 10
-INTS_IN = 4000
-NU = np.array(10.0).reshape([1,])
+INTS_OUT = 100
+INTS_IN = 2000
+NU = np.array(1e-2).reshape([1,])
 
 # First build the computation graph
 
@@ -74,18 +75,26 @@ with tf.variable_scope('Decoder') as dec:
 with tf.variable_scope(dec, reuse=True):
 	x_H = decoder(binary)
 
-# weights = tf.get_default_graph().get_tensor_by_name(
-#   os.path.split(x.name)[0] + '/kernel:0')
 
-train_error = tf.nn.l2_loss(X - x_B)**2 + nu*tf.nn.l2_loss(B - binary)**2 
-# + 0.1*tf.nn.l2_loss(weights)**2
-tf.summary.scalar('train_loss', train_error)
 
-test_error = tf.nn.l2_loss(X- x_H)**2
-tf.summary.scalar('test_loss', test_error)
+train_error = (tf.nn.l2_loss(X - x_B) + tf.reduce_sum(tf.abs(X - x_B))) + nu*(tf.nn.l2_loss(B - binary) + tf.reduce_sum(tf.abs(B - binary))) 
+train_sum = tf.squeeze(train_error) 
 
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
+
+tf.summary.scalar('train_error', train_sum)
+
+test_error = tf.reduce_mean((X - x_H)**2)
+test_sum = tf.squeeze(test_error)
+tf.summary.scalar('test_error', test_sum)
+
+bin_error = (X - x_B)**2
+# final_error = (X - x_H)**2
+
+# optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
+optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-2)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-4)
 train_step = optimizer.minimize(train_error)
+pre_train_step = optimizer.minimize(test_error)
 
 # Now initialize the graph 
 sess = tf.Session()
@@ -95,14 +104,24 @@ writer = tf.summary.FileWriter('./tb',sess.graph)
 
 sess.run(tf.global_variables_initializer())
 
+Xd = np.arange(-1.5, 1.5, 0.01)
+Xd = Xd.reshape([-1,1])
+
+# Initialize with pure SGD
+print('Pre-training...')
+for _ in range(4000):
+	_ = sess.run(pre_train_step, feed_dict={X:Xd})
+
 # Solve the problem... 
 print('Generating binary permutations...')
 Bmax = maxset(BITS)
 print('Binary permutations complete, starting iterations...')
-Xd = np.arange(-1.5, 1.5, 0.01)
-Xd = Xd.reshape([-1,1])
+
 iter_cnt = 0
-for i in range(INTS_OUT):
+done = False 
+tst_err_0 = 1e8
+i = 0
+while not done:
 	c = 0.0
 	print('Iteration %r started...' % (i+1))
 	params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope='Decoder')
@@ -111,21 +130,29 @@ for i in range(INTS_OUT):
 			W = sess.run(param)
 		elif 'bias:' in param.name: 
 			c = sess.run(param)
+	h = sess.run(binary, feed_dict={X:Xd})
 
 	print('Solving for B vector...')
-	b = solveB(Xd, W, c, Bmax)
+	b = solveB(h, Xd, W, c, Bmax, NU)
 	print('B vector found. Begining SGD iterations...')
 	for j in range(INTS_IN):
-		pdb.set_trace()
-		b = b.reshape([1,8])
 		feed_dict = {X:Xd, B:b, nu:NU}
 		_, tr_err, tst_err, summary = sess.run([train_step, train_error, test_error, merged], feed_dict=feed_dict)
 		writer.add_summary(summary, iter_cnt)
 		iter_cnt += 1
-		if (j+1)%500 == 0:
-			print('SGD iteration %r complete with training error %r' % (j+1, tr_err))
-	print('Iteration %r complete with train error %r and test error %r' % (i+1, tr_err, tst_err))
-
+		if j%500 == 0:
+			print('SGD iteration %r complete with training error %r' % (j, tr_err[0]))
+	print('Iteration %r complete with train error %r and test error %r' % (i+1, tr_err[0], tst_err))
+	i += 1
+	d_tst_err = np.abs(tst_err - tst_err_0)
+	tst_err_0 = tst_err
+	if d_tst_err <= 1e-3 or i >= INTS_OUT:
+		done = True
+h = sess.run(binary, feed_dict={X:Xd})
+h_ = np.unique(h)
+bin_err = sess.run(bin_error, feed_dict={X:Xd, B:b})
+max_err = np.amax(bin_err)
+pdb.set_trace()
 saver.save(sess, './models/model') #, global_step=e+start_chk)
 print('Training complete, model saved')
 
