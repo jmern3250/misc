5dbb7890513f396ef65b607cb3e1daf17e441f79
John Mern <jmern91@stanford.edu>
2018-05-30 09:36:36 -0700

    udpated MT3

--- AA222/MT3.py
+++ AA222/MT3.py
@@ -30,6 +30,15 @@ class Tree(object):
         self.child = {}
         self.type = None
         self.value = None
+        self.n_child = 0
+        self.id = None
+
+    @property 
+    def n_subtree(self):
+    	value = 1
+    	for child in self.child.values():
+    		value += child.n_subtree
+    	return value
 
     def set_type(self, type_str, const=None):
     	self.type = type_str
@@ -43,6 +52,13 @@ class Tree(object):
     	else: 
     		self.child[0] = Tree()
     		self.child[1] = Tree()
+    		if self.id is None: 
+    			self.child[0].id = '0'
+    			self.child[1].id = '1'
+    		else:
+    			self.child[0].id = self.id + '0'
+    			self.child[1].id = self.id + '1'
+    		self.n_child = 2
 
     def call(self, x1, x2):
     	if self.type == 'add':
@@ -101,7 +117,7 @@ def gen_random():
 		updated = update_children(tree, terminal)
 		done = updated == 0
 		steps += 1
-		terminal = steps >= 10
+		terminal = steps >= 5
 	return tree 
 		
 #### P5 ####
@@ -123,8 +139,90 @@ def gen_str(tree):
 
 #### P6 ####
 
-np.random.seed(0)
-for _ in range(10):
-	tree = gen_random()
-	print()
-	print(gen_str(tree))
+# np.random.seed(0)
+# for _ in range(10):
+# 	tree = gen_random()
+# 	print()
+# 	print(gen_str(tree))
+
+#### P7 #### 
+
+# np.random.seed(2)
+# tree = gen_random()
+# print(gen_str(tree))
+# X = np.arange(-2., 2., 0.01)
+# m = X.shape[0]
+# Z = np.zeros([m,m])
+
+# for i in range(m):
+# 	for j in range(m):
+# 		Z[i,j] = tree.call(X[j], X[i])
+
+# plt.figure()
+# plt.contour(X,X,Z,50)
+# plt.xlabel('x1')
+# plt.ylabel('x2')
+# plt.savefig('./test.png')
+# # plt.show()
+
+#### P8 #### 
+# np.random.seed(2)
+# def L1(tree, f):
+# 	X = np.arange(-2., 2., 0.2)
+# 	m = X.shape[0]
+# 	eta = 1./m**2
+# 	loss = 0.0
+# 	for i in range(m):
+# 		for j in range(m):
+# 			t = tree.call(X[i], X[j])
+# 			s = f((X[i], X[j]))
+# 			err = np.abs(t - s)*eta
+# 			err = np.clip(err, 0, 1e3)
+# 			loss += err
+# 	return loss
+
+#### P9 ####
+# np.random.seed(0)
+# for _ in range(10):
+# 	tree = gen_random()
+# 	loss = L1(tree, f)
+# 	print(gen_str(tree))
+# 	print('L1 loss:', loss)
+# 	print()
+
+#### P10 #### 
+
+np.random.seed(1)
+def sample_node(tree, n_nodes=None):
+	if n_nodes is None:
+		n_nodes = tree.n_subtree
+	logit = np.random.uniform(0.,1.)
+	if logit <= 1./n_nodes:
+		return tree, n_nodes
+	else: 
+		n_nodes -= 1
+		if not tree.child: 
+			return None, n_nodes
+		else: 
+			for child in tree.child.values():
+				node, n_nodes = sample_node(child, n_nodes)
+				if node is not None:
+					return node, n_nodes
+			return node, n_nodes
+
+def mutate(tree):
+	target_node, _ = sample_node(tree)
+	replace_node = gen_random()
+	done = False
+	# import pdb; pdb.set_trace()
+	target_node = replace_node
+
+
+np.random.seed(2)
+tree = gen_random()
+print(gen_str(tree))
+mutate(tree)
+print(gen_str(tree))
+import pdb; pdb.set_trace()
+
+
