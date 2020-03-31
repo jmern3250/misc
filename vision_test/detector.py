975dad48a6c8ca45fe425904303e74ea5d46066f
John Mern <jmern91@stanford.edu>
2020-03-30 19:29:21 -0700

    pre k-means revert

--- vision_test/detector.py
+++ vision_test/detector.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 import cv2
 
 W = 1250
+H_MAX = 650
 ALPHA = 4.951
 BETA = -0.423
+
+B_H = 100
+B_W = 175
 
 SCORE_COLOR = np.array([255, 246, 157]).reshape([1,1,3]) #R, G, B
 LVL_COLOR = np.array([216, 212, 244]).reshape([1,1,3]) #R, G, B
@@ -22,6 +26,12 @@ def detect(img, n_rows=None):
 
 	score_map, lvl_map, name_map, star_map = extract_layers(img)
 
+	# full_map = score_map + lvl_map + name_map + star_map
+	# full_map = full_map.astype(np.float32)*255.
+	full_map = cv2.Canny(img, 100, 200).astype(np.float32)
+	# import pdb; pdb.set_trace()
+	full_map = full_map[:H_MAX, ...]
+
 	if n_rows is None:
 		ar = h/w
 		n_rows = np.round(ALPHA*ar + BETA).astype(np.int32)
@@ -30,42 +40,45 @@ def detect(img, n_rows=None):
 	else:
 		raise ValueError('n_rows must be a numeric value or "None"')
 
-	h, w, _ = img.shape
-	m0 = b0 = h//(n_rows + 1.)
-	c_y_score, m, b = detect_rows(score_map.astype(np.float64)*255., r_est_linear, 10, m0, b0)
-	c_x = detect_cols(score_map.astype(np.float64)*255.)
+	c_y, m, b = detect_rows(full_map, n_rows, 10)
+	c_x = detect_cols(full_map)
 
-	plt.imshow(img)
-	for x in c_x:
-		for y in c_y:
-			plt.plot(x, y, 'ro')
-	plt.show()
+	return segment_image(img, c_x, c_y)
 
-
-def detect_rows(layer, n_rows, n_its, m0, b0):
+def detect_rows(layer, n_rows, n_its):
 	layer = (layer==255).astype(np.float64)
 	m, n = layer.shape
-	cur_intercept = b0
-	cur_slope = m0
+	cur_slope = m/2
+	cur_intercept = m/2/2 + 20 
 	_, y_grid = np.meshgrid(np.arange(n), np.arange(m))
 	y_grid = np.expand_dims(y_grid, axis=-1)
+	d_intercept = 0 
+	d_slope = 0 
 	for i in range(n_its):
-		intercepts = [cur_intercept - 5, cur_intercept - 1 , cur_intercept, cur_intercept + 1, cur_intercept + 5]
-		slopes = [cur_slope - 5, cur_slope - 1 , cur_slope, cur_slope + 1, cur_slope + 5]
+		intercepts = 2*(np.arange(7) - 3.) + np.clip(d_intercept, -5, 5) + cur_intercept 
+		slopes = 2*(np.arange(7) - 3.) + np.clip(d_slope, -5, 5) + cur_slope 
 		min_error = np.inf
+		d_slope = 0
+		d_intercept = 0 
 		for intercept in intercepts:
 			for slope in slopes:
-				centroids = np.arange(n_rows)*slope + intercept 
-				centroids = centroids.reshape((1, 1, n_rows))
+				centroids = np.arange(2)*slope + intercept 
+				centroids = centroids.reshape((1, 1, 2))
 				##### Assign Pixel Clusters #####
-				d = np.square(y_grid - centroids)
+				d = np.abs(y_grid - centroids)
 				d_min = np.min(d, axis=-1)
 				d_min *= layer
+				mask = d_min >= 20
+				d_min *= mask.astype(np.float32)
 				error = np.sum(d_min**2)
 				if error < min_error:
+					d_slope += slope - cur_slope
+					d_intercept += intercept - cur_intercept
 					min_error = error
 					cur_slope = slope
 					cur_intercept = intercept
+		if d_slope == 0 and d_intercept == 0: 
+			break
 
 	c_rows = np.arange(n_rows)*cur_slope + cur_intercept
 	return c_rows, cur_slope, cur_intercept
@@ -73,15 +86,18 @@ def detect_rows(layer, n_rows, n_its, m0, b0):
 def detect_cols(layer):
 	layer = (layer==255).astype(np.float64)
 	m, n = layer.shape
-	cur_intercept = n//(5. + 1)
-	cur_slope = n//(5 + 1.)
+	cur_intercept = n/5/2
+	cur_slope = n/5
 	x_grid, _ = np.meshgrid(np.arange(n), np.arange(m))
 	x_grid = np.expand_dims(x_grid, axis=-1)
+	d_intercept = 0 
+	d_slope = 0 
 	for i in range(10):
-		intercepts = [cur_intercept - 10, cur_intercept - 1 , cur_intercept, cur_intercept + 1, cur_intercept + 10]
-		slopes = [cur_slope - 10, cur_slope - 1 , cur_slope, cur_slope + 1, cur_slope + 10]
+		intercepts = 2*(np.arange(7) - 3.) + cur_intercept + np.clip(d_intercept, -5, 5)
+		slopes = 2*(np.arange(7) - 3.) + cur_slope + np.clip(d_slope, -5, 5)
 		min_error = np.inf
-		max_score = 0
+		d_slope = 0
+		d_intercept = 0 
 		for intercept in intercepts:
 			for slope in slopes:
 				centroids = np.arange(5)*slope + intercept 
@@ -92,10 +108,13 @@ def detect_cols(layer):
 				d_min *= layer
 				error = np.sum(d_min**2)
 				if error < min_error:
+					d_slope += slope - cur_slope
+					d_intercept += intercept - cur_intercept
 					min_error = error
 					cur_slope = slope
 					cur_intercept = intercept
-
+		if d_slope == 0 and d_intercept == 0: 
+			break
 	c_cols = np.arange(5)*cur_slope + cur_intercept
 	return c_cols
 
@@ -147,44 +166,39 @@ def extract_layers(pic):
 	
 	return score_map, lvl_map, name_map, star_map
 
-def detect_layer(layer, n_rows, d_x, d_y):
-	m, n = layer.shape
-	y_initial = np.linspace(0, m, n_rows + 2)[1:-1]
-	y_initial = y_initial.astype(np.int32)
-	x_initial = np.linspace(0, n, 7)[1:-1]
-	x_initial = x_initial.astype(np.int32)
+def segment_image(img, c_x, c_y):
+	imgs = []
+	for y in c_y:
+		for x in c_x:
+			start_y = int(y-B_H//2)
+			end_y = start_y + B_H
+			start_x = int(x-B_W//2)
+			end_x = start_x + B_W
+			img_seg = img[start_y:end_y, start_x:end_x, ...]
+			plt.imshow(img_seg)
+			plt.show()
+			imgs.append(img_seg)
+	return imgs
 
-	n_items = len(x_initial)*len(y_initial)
-	mask = np.zeros((m, n))
-	centroids = []
-	for x_c in x_initial:
-		for y_c in y_initial:
-			mask[y_c-d_y:y_c+d_y, x_c-d_x:x_c+d_x] = 1
-			centroids.append((x_c, y_c))
-	layer = np.round(layer*mask)
-	x_grid, y_grid = np.meshgrid(np.arange(n), np.arange(m))
-	for i in range(3):
-		##### Assign Pixel Clusters #####
-		d = np.zeros((m,n,n_items))
-		for j, centroid in enumerate(centroids):
-			d_x_s = (x_grid - centroid[0])**2
-			d_y_s = (y_grid - centroid[1])**2
-			d_c = np.sqrt(d_x_s + d_y_s)
-			d[...,j] = d_c
-		idxs = np.argmin(d, axis=-1) + 1.0 # 1 index the centroids
-		idxs *= layer # Mask out the zero-pixels
-		centroids_old = centroids
-		centroids = []
-		##### Update Centroid Location #####
-		for j, centroid in enumerate(centroids_old):
-			grid_idxs = np.argwhere(idxs == (j+1))
-			centroid_rc = np.mean(grid_idxs, axis=0)
-			centroids.append((centroid_rc[1], centroid_rc[0]))
-	return np.array(centroids).astype(np.int32)
-
+# def segment_image(img, n_rows):
+# 	imgs = []
+# 	m, n, _ = img.shape
+# 	ys = np.linspace(0, m, n_rows+1)
+# 	xs = np.linspace(0, n, 6)
+# 	for i in range(n_rows):
+# 		for j in range(5):
+# 			x_start = int(xs[j])
+# 			x_end = int(xs[j + 1])
+# 			y_start = int(ys[i])
+# 			y_end = int(ys[i + 1])
+# 			img_seg = img[y_start:y_end, x_start:x_end, ...]
+# 			plt.imshow(img_seg)
+# 			plt.show()
+# 			imgs.append(img_seg)
+# 	return imgs
 
 if __name__ == "__main__":
-	img = cv2.imread('data/pic3.jpg')
-	# img = cv2.imread('data/pic4.png')
+	# img = cv2.imread('data/pic3.jpg')
+	img = cv2.imread('data/pic5.png')
 	detect(img)
 	
